ac8494bb6e4a2128d454b4b60ca314c81e9b1f6d
Ricardo <rick.hc3@gmail.com>
2020-12-06 19:08:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from "./components/Comments";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "app",
+  components: {
+    Comments,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-todo form-group">
+  <p>
+    <input placeholder="Nome" type="text" name="author" class="form-control" v-model="name" />
+  </p>
+    <p>
+    <input placeholder="Idade" type="number" name="author" class="form-control" v-model="idade" />
+  </p>
+  <p>
+    <textarea placeholder="Comentário" name="message"  class="form-control" v-model="message"></textarea>
+  </p>
+  <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        message: ''
+      }
+    },
+    methods: {
+      addComment() {
+        if (this.message.trim() === '') {
+          return;
+        }
+        this.$emit('add-todo', {
+          name: this.name,
+          idade: this.idade,
+          message: this.message,
+        });
+        this.name = '';
+        this.idade = '';
+        this.message = '';
+      }
+    }
+  }
+</script>
